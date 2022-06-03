b5f5fadf033b8f8892cb0d50779da71a3bc85b4e
Oguz Ayric <oguzayrc@gmail.com>
2022-06-03 15:20:56 +0300

    Add project files.

---
+++ TodoListApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TodoListApi.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TodoItem>().HasData(
+                new TodoItem() { Id = 1, IdDone = true, Title="Do your homework" },
+                new TodoItem() { Id = 2, IdDone = false, Title = "Do fitness" },
+                new TodoItem() { Id = 3, IdDone = false, Title = "Call you parents" },
+                new TodoItem() { Id = 4, IdDone = true, Title = "Clean your room" }
+                );
+        }
+    }
+}
